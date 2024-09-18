dd9e2f7dd9e4e394f1920a0ce5f2a5ceb19169ee
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-18 11:36:06 +0530

    feat: export types Node and Stack for documentation

--- ui/navigator/stack.go
+++ ui/navigator/stack.go
@@ -1,55 +1,55 @@
 package navigator
 
 // Node is an element in the linked list
-type node[T any] struct {
+type Node[T any] struct {
 	data T
-	next *node[T]
+	next *Node[T]
 }
 
-// [newNode] returns a new node instance of type [T]
-func newNode[T any](data T, next *node[T]) *node[T] {
-	return &node[T]{
+// [NewNode] returns a new node instance of type [T]
+func NewNode[T any](data T, next *Node[T]) *Node[T] {
+	return &Node[T]{
 		data: data,
 		next: next,
 	}
 }
 
 // Linked list based stack for navigation purposes
-type stack[T any] struct {
+type Stack[T any] struct {
 	length int
-	top    *node[T]
+	top    *Node[T]
 }
 
 // [NewStack] returns a new stack instance of type [T]
-func NewStack[T any]() *stack[T] {
-	return &stack[T]{
-		top: newNode(*new(T), nil),
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{
+		top: NewNode(*new(T), nil),
 	}
 }
 
 // [NewStackWithData] takes in the first element in the stack returns a new stack instance of type [T]
-func NewStackWithData[T any](data T) *stack[T] {
-	return &stack[T]{
+func NewStackWithData[T any](data T) *Stack[T] {
+	return &Stack[T]{
 		length: 1,
-		top:    newNode(data, nil),
+		top:    NewNode(data, nil),
 	}
 }
 
 // [Len] returns the length of the stack
-func (s *stack[T]) Len() int {
+func (s *Stack[T]) Len() int {
 	return s.length
 }
 
 // [Push] adds an element to the top of the stack
-func (s *stack[T]) Push(element T) {
-	newElement := newNode(element, s.top)
+func (s *Stack[T]) Push(element T) {
+	newElement := NewNode(element, s.top)
 	s.top = newElement
 
 	s.length++
 }
 
 // [Pop] removes the top element from the stack and a boolean indicating if the stack is empty
-func (s *stack[T]) Pop() (T, bool) {
+func (s *Stack[T]) Pop() (T, bool) {
 	if s.length == 0 {
 		return *new(T), false
 	}
@@ -65,7 +65,7 @@ func (s *stack[T]) Pop() (T, bool) {
 }
 
 // [Peek] returns the top element of the stack and a boolean indicating if the stack is empty
-func (s *stack[T]) Peek() (T, bool) {
+func (s *Stack[T]) Peek() (T, bool) {
 	if s.length == 0 {
 		return *new(T), false
 	}
