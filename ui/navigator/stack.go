2422f475d8f106ff4623f9942b918c985616e8cd
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-18 11:49:07 +0530

    refactor: improve consistency in stack documentation

--- ui/navigator/stack.go
+++ ui/navigator/stack.go
@@ -1,6 +1,6 @@
 package navigator
 
-// Node is an element in the linked list
+// Node is an element in the [Stack]
 type Node[T any] struct {
 	data T
 	next *Node[T]
@@ -16,59 +16,59 @@ func NewNode[T any](data T, next *Node[T]) *Node[T] {
 
 // Linked list based stack for navigation purposes
 type Stack[T any] struct {
-	length int
-	top    *Node[T]
+	Length int
+	Top    *Node[T]
 }
 
 // [NewStack] returns a new [Stack] instance of type [T]
 func NewStack[T any]() *Stack[T] {
 	return &Stack[T]{
-		top: NewNode(*new(T), nil),
+		Top: NewNode(*new(T), nil),
 	}
 }
 
 // [NewStackWithData] takes in the first element in the [Stack] & returns a new [Stack] instance of type [T]
 func NewStackWithData[T any](data T) *Stack[T] {
 	return &Stack[T]{
-		length: 1,
-		top:    NewNode(data, nil),
+		Length: 1,
+		Top:    NewNode(data, nil),
 	}
 }
 
 // [Len] returns the length of the [Stack]
 func (s *Stack[T]) Len() int {
-	return s.length
+	return s.Length
 }
 
 // [Push] adds an element to the top of the [Stack]
 func (s *Stack[T]) Push(element T) {
-	newElement := NewNode(element, s.top)
-	s.top = newElement
+	newElement := NewNode(element, s.Top)
+	s.Top = newElement
 
-	s.length++
+	s.Length++
 }
 
 // [Pop] removes the top element from the [Stack] and a boolean indicating if the [Stack] is empty
 func (s *Stack[T]) Pop() (T, bool) {
-	if s.length == 0 {
+	if s.Length == 0 {
 		return *new(T), false
 	}
 
-	prevTop := s.top
+	prevTop := s.Top
 	value := prevTop.data
 
-	s.top = prevTop.next
+	s.Top = prevTop.next
 	prevTop = nil
 
-	s.length--
+	s.Length--
 	return value, true
 }
 
 // [Peek] returns the top element of the [Stack] and a boolean indicating if the [Stack] is empty
 func (s *Stack[T]) Peek() (T, bool) {
-	if s.length == 0 {
+	if s.Length == 0 {
 		return *new(T), false
 	}
 
-	return s.top.data, true
+	return s.Top.data, true
 }
