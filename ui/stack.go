df65ea11fa9ad788d93c21f27f59a84ce4ba0164
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-18 09:37:53 +0530

    feat: add stack implementation for navigation purposes

---
+++ ui/stack.go
@@ -0,0 +1,57 @@
+package ui
+
+type node[T any] struct {
+	data T
+	next *node[T]
+}
+
+// Linked list based stack for navigation purposes
+type stack[T any] struct {
+	length int
+	head   *node[T]
+	tail   *node[T]
+}
+
+// [NewStack] returns a new stack instance with type [T]
+func NewStack[T any]() *stack[T] {
+	return &stack[T]{
+		head: &node[T]{},
+		tail: &node[T]{},
+	}
+}
+
+func (s *stack[T]) Len() int {
+	return s.length
+}
+
+func (s *stack[T]) Push(element T) {
+	newElement := &node[T]{data: element}
+	defer func() {
+		s.length += 1
+	}()
+
+	if s.length > 0 {
+		s.tail.next = newElement
+		s.tail = newElement
+		return
+	}
+
+	s.head = newElement
+	s.tail = newElement
+}
+
+func (s *stack[T]) Pop() T {
+	defer func() {
+		s.length -= 1
+	}()
+
+	currentHead := s.head
+	s.head = s.head.next
+
+	currentHead.next = nil
+	return currentHead.data
+}
+
+func (s *stack[T]) Peek() T {
+	return s.head.data
+}
