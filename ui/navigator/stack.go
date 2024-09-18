5e28f6caeb4aeffeea2c7a2d388f78d2f9d0fb86
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-09-18 11:41:28 +0530

    refactor: improve consistency in stack docs

--- ui/navigator/stack.go
+++ ui/navigator/stack.go
@@ -6,7 +6,7 @@ type Node[T any] struct {
 	next *Node[T]
 }
 
-// [NewNode] returns a new node instance of type [T]
+// [NewNode] returns a new [Node] instance of type [T]
 func NewNode[T any](data T, next *Node[T]) *Node[T] {
 	return &Node[T]{
 		data: data,
@@ -20,14 +20,14 @@ type Stack[T any] struct {
 	top    *Node[T]
 }
 
-// [NewStack] returns a new stack instance of type [T]
+// [NewStack] returns a new [Stack] instance of type [T]
 func NewStack[T any]() *Stack[T] {
 	return &Stack[T]{
 		top: NewNode(*new(T), nil),
 	}
 }
 
-// [NewStackWithData] takes in the first element in the stack returns a new stack instance of type [T]
+// [NewStackWithData] takes in the first element in the [Stack] & returns a new [Stack] instance of type [T]
 func NewStackWithData[T any](data T) *Stack[T] {
 	return &Stack[T]{
 		length: 1,
@@ -35,12 +35,12 @@ func NewStackWithData[T any](data T) *Stack[T] {
 	}
 }
 
-// [Len] returns the length of the stack
+// [Len] returns the length of the [Stack]
 func (s *Stack[T]) Len() int {
 	return s.length
 }
 
-// [Push] adds an element to the top of the stack
+// [Push] adds an element to the top of the [Stack]
 func (s *Stack[T]) Push(element T) {
 	newElement := NewNode(element, s.top)
 	s.top = newElement
@@ -48,7 +48,7 @@ func (s *Stack[T]) Push(element T) {
 	s.length++
 }
 
-// [Pop] removes the top element from the stack and a boolean indicating if the stack is empty
+// [Pop] removes the top element from the [Stack] and a boolean indicating if the [Stack] is empty
 func (s *Stack[T]) Pop() (T, bool) {
 	if s.length == 0 {
 		return *new(T), false
@@ -64,7 +64,7 @@ func (s *Stack[T]) Pop() (T, bool) {
 	return value, true
 }
 
-// [Peek] returns the top element of the stack and a boolean indicating if the stack is empty
+// [Peek] returns the top element of the [Stack] and a boolean indicating if the [Stack] is empty
 func (s *Stack[T]) Peek() (T, bool) {
 	if s.length == 0 {
 		return *new(T), false
