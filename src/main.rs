445efc6cb1ef8626fac6e7c9d2182744d0906ea8
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-02-24 01:54:26 +0530

    feat: add YTDLP lazy static variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use once_cell::sync::Lazy;
+use ytdlp::YTDlp;
+
+mod ytdlp;
+
+const BROOK_DIR_NAME: &str = ".brook";
+const ASCII_ART: &str = r#"
+ _                     _
+| |                   | |
+| |__  ____ ___   ___ | | __
+|  _ \|  __/ _ \ / _ \| |/ /
+| |_) | | | (_) | (_) |   <
+|____/|_|  \___/ \___/|_|\_\
+"#;
+
+pub static YTDLP: Lazy<YTDlp> = ytdlp::init();
+
 fn main() {
-    println!("Hello, world!");
+    println!("{ASCII_ART}");
+
+    // regardless of how many times this is called,
+    // it will only be executed once
+    YTDLP.sync_version();
+    YTDLP.sync_version();
+
+    // println!("__, YTDLP_TEMP_PATH: -> {:#?}", YTDLP);
 }
